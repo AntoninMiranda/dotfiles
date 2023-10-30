296fb55bbbc0afce5f01030a1cf04596696026da
AntoninMiranda <antonin.miranda@epitech.eu>
2023-10-30 13:47:14 +0100

    [+] saver for dotfiles

---
+++ save_dotfiles.sh
@@ -0,0 +1,6 @@
+cp -r ~/.config/i3/* ~/Dotfiles/.config/i3/
+cp -r ~/.config/polybar/* ~/Dotfiles/.config/polybar/
+cp -r ~/.config/kitty/* ~/Dotfiles/.config/kitty/
+cp -r ~/.config/nvim/* ~/Dotfiles/.config/nvim/
+rm -rf ~/Dotfiles/.config/nvim/lua/nemo/undodir/*
+
